7a30c2038cf591799f0846ec00727e7659fa2d59
Nikita Loginov <nikitaloginov264@gmail.com>
2025-03-17 21:45:17 +0300

    начала проекта, базовая настройка

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+
+import '../styles/reset.scss';
+import '../styles/global.scss';
+import '../styles/vars.scss';
+
+const NunitoFont = Nunito({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dodo Clone",
+  description: "Онлайн-магазин по продаже еды",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={`${NunitoFont.variable}`}>
+        <div className="wrapper">
+          <header className="header"></header>
+
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>jfdkfls</h1>
+    </>
+  );
+}
